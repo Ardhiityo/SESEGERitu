d64ea5689960d446bf05046e8b95672f497a7ff4
Ardhityo <aryaadi229@gmail.com>
2026-06-23 00:12:33 +0700

    feat: add link social media

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -5,8 +5,9 @@ import {
   Globe,
   Link as LinkIcon,
   Music,
-  Mail,
   Link,
+  Store,
+  ShoppingBag,
 } from "lucide-react";
 
 export default function Social() {
@@ -58,6 +59,20 @@ export default function Social() {
           >
             <Link className="w-5 h-5"></Link>
             Linktree
+          </a>
+          <a
+            href="https://shopee.co.id/sesegeritu"
+            className="flex items-center gap-2 rounded-full bg-card border border-border/40 px-5 py-3 font-medium hover:bg-primary hover:text-primary-foreground hover:-translate-y-1 transition-all"
+          >
+            <ShoppingBag className="w-5 h-5"></ShoppingBag>
+            Shopee
+          </a>
+          <a
+            href="https://www.tokopedia.com/sesegeritu"
+            className="flex items-center gap-2 rounded-full bg-card border border-border/40 px-5 py-3 font-medium hover:bg-primary hover:text-primary-foreground hover:-translate-y-1 transition-all"
+          >
+            <Store className="w-5 h-5"></Store>
+            Tokopedia
           </a>
         </div>
       </div>
